6fb7784849b8303b9e454d5ad97f544e038dfd26
samrat18 <samrathalder18@gmail.com>
2017-02-09 11:46:35 +0530

    updated with functions

--- SMGPE/GP_1d.py
+++ SMGPE/GP_1d.py
@@ -1,68 +1,72 @@
 import numpy as np
-from matplotlib.pyplot import *
-from roots_test import h_roots_recursive
-from roots_test import hermite_on
+from matplotlib import pyplot as plt
+from roots_test import h_roots_recursive, hermite_on
 import odespy as os
 
-n = 20
+nbase = 40
 lamda = 100
 T = 100
 dt = 0.005
-steps = T/0.005
+tsteps = T/0.005
 h = 1
-space_grid = 256
-x_space = np.linspace(-8,8,space_grid)
+Nx = 256
+x_low = -8.
+x_up = 8.
+x_grid = np.linspace(x_low,x_up,Nx)
 
-
-C_init = np.zeros(n+1)+ 0j
+C_init = np.zeros(nbase+1)+ 0j
 C_init[0] = 1
 
-x1, w1 = h_roots_recursive(2*n+1)
+x1, w1 = h_roots_recursive(2*nbase+1)
 
-#C = np.zeros((n+1, steps+1))+ 0j
-P_nk = np.zeros((n+1,2*n+1))+ 0j
+def effOfC(n, x, w, wave_init):
+    P_nk = np.zeros((n+1,2*n+1))+ 0j
+    eProots = np.exp(-(x**2.)/2)
+    weights = w * eProots
 
-eProots = np.exp(-(x1**2.)/2)
-weights = w1 * eProots
+    for i in xrange(n+1):
+        P_nk[i,] = hermite_on(i)(x1) * eProots
 
-for i in xrange(n+1):
-    P_nk[i,] = hermite_on(i)(x1) * eProots
+    psi = np.dot(P_nk.T, wave_init)
 
-psi = np.dot(P_nk.T, C_init)
+    xi = weights * abs(psi)**2. * psi
+    effC = np.dot(P_nk, xi)
+    return effC
 
-xi = weights * abs(psi)**2. * psi
-effC = np.dot(P_nk, xi)
+effC = effOfC(nbase, x1, w1, C_init)
 
-def f(u,t):
-    return -1j* (h*u+ lamda * effC)
+def rk4(wave_init, h, lamda, eff_C, Tmax, Nt):
+    def f(u,t):
+        return -1j* (h*u+ lamda * eff_C)
 
-solver = os.RK4(f, complex_valued = True)
-solver.set_initial_condition(C_init)
+    solver = os.RK4(f, complex_valued = True)
+    solver.set_initial_condition(wave_init)
 
-time_points = np.linspace(0,T, steps)
-u, t = solver.solve(time_points)
+    time_points = np.linspace(0,Tmax, Nt)
+    u, t = solver.solve(time_points)
+    return u, t
+
+y, t = rk4(C_init, h, lamda, effC, T, tsteps)
 
 def phi(n, x_space):
-    phi_n = np.zeros((n+1,space_grid)) + 0j
+    phi_n = np.zeros((n+1, len(x_space))) + 0j
     for i in xrange(n+1):
         phi_n[i,:] = hermite_on(i)(x_space)*np.exp(-(x_space**2.))
     return phi_n
 
-Phii = phi(n, x_space)
+Phii = phi(nbase, x_grid)
 
-#print np.shape(Phii)
-#print np.shape(u)
-
-wave_func = np.dot(u,Phii)
-#wave_func = np.sum( u* Phii, axis =1)
+wave_func = np.dot(y,Phii)
 density = np.abs(wave_func)**2.
 
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.plot(x_grid, density[tsteps-1,:], 'b-')
+ax2 = fig.add_subplot(212)
+ax2.plot(t, density[:,Nx/2], 'r-')
+plt.show()
 
+#wave_func = np.sum( u* Phii, axis =1)
 #plot(time_points, density, 'r-')
-
-plot(x_space, density[steps-1,:], 'b-')
-
 #savefig('lamda=1000.png')
-show()
 
-#print np.shape(u)
